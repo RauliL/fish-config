c389b5f18bfb4b72b49b10ced3011bff758096ff
Rauli Laine <rauli.laine@anders.fi>
2017-09-14 12:38:03 +0300

    Add wo function for Python projects

---
+++ conf.d/wo.fish
@@ -0,0 +1,15 @@
+function wo
+    set -l project $argv[1]
+    set -l project_dir "$HOME/project/$project"
+    set -l venv_dir "$HOME/project/venv/$project"
+
+    if [ -d "$project_dir" -a -d "$venv_dir" ]
+        if [ -n "$VIRTUAL_ENV" ]
+            deactivate
+        end
+        source "$venv_dir/bin/activate.fish"
+        cd "$project_dir"
+    else
+        echo "Unrecognized project: $project"
+    end
+end
