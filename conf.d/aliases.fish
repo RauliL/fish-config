802ad8fd1294e6265ad97b0cdbaf44a6c57e6311
Rauli Laine <rauli.laine@iki.fi>
2018-09-01 09:09:52 +0300

    Omit output from bat discovery

--- conf.d/aliases.fish
+++ conf.d/aliases.fish
@@ -6,6 +6,6 @@ alias x='exit'
 
 # Use bat instead of cat, if it's installed on the system.
 # See: https://github.com/sharkdp/bat
-if which bat
+if which bat > /dev/null 2>&1
     alias cat='bat'
 end
