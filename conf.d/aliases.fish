ddbaab4af1ab9ce468c09627866d2eea118857c9
Rauli Laine <rauli.laine@iki.fi>
2026-04-27 14:59:07 +0300

    Bring back some aliases I have gotten used to

---
+++ conf.d/aliases.fish
@@ -0,0 +1,16 @@
+alias g=git
+alias x=exit
+
+# Use bat instead of cat, if it's available.
+# See: https://github.com/sharkdp/bat
+if which batcat > /dev/null 2>&1
+  alias cat=batcat
+else if which bat > /dev/null 2>&1
+  alias cat=bat
+end
+
+# Use exa instead of ls, if it's available.
+# See: https://github.com/ogham/exa
+if which exa > /dev/null 2>&1
+  alias ls=exa
+end
