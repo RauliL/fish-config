546e77edc5f5a27305ebb84308ea6f0da4a6fe22
Rauli Laine <rauli.laine@iki.fi>
2026-04-27 14:54:59 +0300

    Do a hard reset

--- conf.d/starship.fish
+++
@@ -1,4 +0,0 @@
-# Use starship.rs, if it's installed on the system.
-if which starship > /dev/null 2>&1
-    starship init fish | source
-end
--- conf.d/variables.fish
+++
@@ -1,3 +0,0 @@
-set -gx EDITOR nvim
-set -gx VISUAL nvim
-set -gx BROWSER firefox
